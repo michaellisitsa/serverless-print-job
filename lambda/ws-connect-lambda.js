c1421156f2c733a00e1b6a02c4bd1d2228a44df0
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-01-12 22:13:02 +1100

    Remove dynamo db from connection and disconnection handlers

--- lambda/ws-connect-lambda.js
+++ lambda/ws-connect-lambda.js
@@ -1,25 +1,5 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
-
-// Import the DynamoDB Document Client.
-const dynamoDBClient = new DynamoDBClient({ region: process.env.AWS_REGION });
-
-// Maps JS types to dynamoDB attributevalue.
-const dynamoDBDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
-
 exports.handler = async (event) => {
   console.log("Connection Event:", JSON.stringify(event));
 
-  const putCommand = new PutCommand({
-    TableName: process.env.TABLE_NAME,
-    Item: {
-      jobId: `PrintJob-${event.requestContext.connectionId}`,
-      connectionId: event.requestContext.connectionId,
-      status: "pending",
-    },
-  });
-
-  const response = await dynamoDBDocClient.send(putCommand);
-  console.log("putCommand response:", JSON.stringify(response));
   return { statusCode: 200 };
 };
--- lambda/ws-disconnect-lambda.js
+++ lambda/ws-disconnect-lambda.js
@@ -1,28 +1,5 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const {
-  DynamoDBDocumentClient,
-  UpdateCommand,
-} = require("@aws-sdk/lib-dynamodb");
-
-// Import the DynamoDB Document Client.
-const dynamoDBClient = new DynamoDBClient({ region: process.env.AWS_REGION });
-
-// Maps JS types to dynamoDB attributevalue.
-const dynamoDBDocClient = DynamoDBDocumentClient.from(dynamoDBClient);
-
 exports.handler = async (event) => {
   console.log("Disconnection Event:", JSON.stringify(event));
-
-  const updateCommand = new UpdateCommand({
-    TableName: process.env.TABLE_NAME,
-    Key: {
-      jobId: `PrintJob-${event.requestContext.connectionId}`,
-    },
-    UpdateExpression: "REMOVE connectionId",
-  });
-
-  const resp = await dynamoDBDocClient.send(updateCommand);
-  console.log(`deleteCommand resp => ${JSON.stringify(resp)}`);
 
   return { statusCode: 200 };
 };
